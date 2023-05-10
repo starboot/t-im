395361fd48d0a77db8f2ff631ebbacf4869dfc3a
MDong <1191998028@qq.com>
2023-05-10 20:09:29 +0800

    updating

--- t-im-common/src/main/java/cn/starboot/tim/common/exception/ImDecodeException.java
+++
@@ -1,6 +0,0 @@
-package cn.starboot.tim.common.exception;
-
-import cn.starboot.socket.exception.AioDecoderException;
-
-public class ImDecodeException extends AioDecoderException {
-}
--- t-im-common/src/main/java/cn/starboot/tim/common/exception/ImEncodeException.java
+++
@@ -1,6 +0,0 @@
-package cn.starboot.tim.common.exception;
-
-import cn.starboot.socket.exception.AioEncoderException;
-
-public class ImEncodeException extends AioEncoderException {
-}
